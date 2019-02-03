0b1ab35d1f6b3aea2b53af778190d19e2e336de3
ShouyaToyama <shouya.toyama@gmail.com>
2019-02-03 12:33:07 +0900

    Add semantic-ui-react.js and css

--- project/frontend/src/components/Item.js
+++ project/frontend/src/components/Item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
+import { Item } from "semantic-ui-react";
 
 const ItemList = ({data}) => 
     !data.length ? (
